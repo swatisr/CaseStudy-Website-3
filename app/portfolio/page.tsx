2805bdb8b29519745884c09be13eb56298493124
Your Name <you@company.com>
2025-12-18 22:20:07 +0530

    Initial commit: CaseStudy Website 3

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,10 +3,8 @@ import ProjectSection from "@/components/ProjectSection"
 export default function PortfolioPage() {
   return (
     <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
-      {/* 4 Project Sections */}
-      {[1, 2, 3, 4].map((index) => (
-        <ProjectSection key={index} index={index} />
-      ))}
+      {/* Single Project Section */}
+      <ProjectSection index={1} />
       
       {/* 5th Section - Skills (placeholder) */}
       <section className="flex h-[532px] snap-start items-center justify-center p-12">
